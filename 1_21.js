08a218686686963429f622d8d5b1768ba8535a32
BacroXP <135527354+BacroXP@users.noreply.github.com>
2024-12-17 20:44:43 +0100

    Update 1_21.js

--- 1_21.js
+++ 1_21.js
@@ -1,1 +1,21 @@
-
+[
+  {
+    "id": 0,
+    "name": "air",
+    "displayName": "Air",
+    "hardness": 0.0,
+    "resistance": 0.0,
+    "stackSize": 64,
+    "diggable": false,
+    "material": "default",
+    "transparent": true,
+    "emitLight": 0,
+    "filterLight": 0,
+    "defaultState": 0,
+    "minStateId": 0,
+    "maxStateId": 0,
+    "states": [],
+    "drops": [],
+    "boundingBox": "empty"
+  }
+]
